070e059f372731af614492140c3741516086051d
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-03-04 21:32:07 +0300

    Add Brain even game

---
+++ src/games/even.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import { sayGreetings } from '../gameEngine.js';
+import { getRandomNumber, isEven } from '../utils.js';
+
+const even = () => {
+  const username = sayGreetings();
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomNumber();
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${username}`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${username}!`);
+};
+
+export default even;
---
+++ src/utils.js
@@ -0,0 +1,4 @@
+const getRandomNumber = () => Math.round(Math.random() * 100);
+const isEven = (number) => number % 2 === 0;
+
+export { getRandomNumber, isEven };
