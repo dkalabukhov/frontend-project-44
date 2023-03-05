f348e4807dc19b0d88cff983bc5773db47588295
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-03-05 09:28:25 +0300

    Fix src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,25 +1,19 @@
-import readlineSync from 'readline-sync';
-import { sayGreetings } from '../index.js';
+import startGame from '../index.js';
 import { getRandomNumber, isEven } from '../utils.js';
 
 const even = () => {
-  const username = sayGreetings();
+  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const questions = [];
+  const correctAnswers = [];
 
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
     const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${username}`);
-      return;
-    }
+    questions.push(randomNumber);
+    correctAnswers.push(correctAnswer);
   }
-  console.log(`Congratulations, ${username}!`);
+
+  startGame(description, questions, correctAnswers);
 };
 
 export default even;
