adb82c61157fff02ba96fe689d1462a97e0098aa
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-03-05 09:25:52 +0300

    Created game engine in src/index.js

--- src/index.js
+++ src/index.js
@@ -8,4 +8,42 @@ const sayGreetings = () => {
   return name;
 };
 
-export { sayGreetings };
+const showGameDescription = (description) => {
+  console.log(description);
+};
+
+const showQuestion = (question) => {
+  console.log(`Question: ${question}`);
+};
+
+const showCongratulations = (username) => {
+  console.log(`Congratulations, ${username}`);
+};
+
+const getAnswer = () => {
+  const answer = readlineSync.question('Your answer: ');
+  return answer;
+};
+
+// description, [questions], [correctAnswers]
+const startGame = (description, questions, correctAnswers) => {
+  const username = sayGreetings();
+  showGameDescription(description);
+
+  for (let i = 0; i < 3; i += 1) {
+    showQuestion(questions[i]);
+    const answer = getAnswer();
+
+    if (answer === correctAnswers[i]) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswers[i]}.`);
+      console.log(`Let's try again, ${username}`);
+      return;
+    }
+  }
+
+  showCongratulations(username);
+};
+
+export default startGame;
