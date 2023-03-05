0d778190f998934dc696f8b6033328f1ba401b69
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-03-05 15:47:22 +0300

    Fix utils.js: getGreatestCommonDivisor function is improved; Fix Makefile: add brain-gcd command

--- src/utils.js
+++ src/utils.js
@@ -12,37 +12,16 @@ const getRandomMathOperator = () => {
   return randomMathOperator;
 };
 
-const getGreatestCommonDivisor = (num1, num2) => {
-  let firstNumber = num1;
-  let secondNumber = num2;
-  let remainder;
-  let counter = true;
-  let result;
+const getGreatestCommonDivisor = (firstNumber, secondNumber) => {
+  let divisor;
 
-  while (counter) {
-    if (firstNumber > secondNumber) {
-      remainder = firstNumber % secondNumber;
-      if (remainder === 0) {
-        result = secondNumber;
-        counter = false;
-      } else {
-        firstNumber = remainder;
-      }
-    } else if (secondNumber > firstNumber) {
-      remainder = secondNumber % firstNumber;
-      if (remainder === 0) {
-        result = firstNumber;
-        counter = false;
-      } else {
-        secondNumber = remainder;
-      }
-    } else if (firstNumber === secondNumber) {
-      result = firstNumber;
-      counter = false;
+  for (let i = 1; i <= firstNumber && i <= secondNumber; i += 1) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      divisor = i;
     }
   }
 
-  return result.toString();
+  return divisor.toString();
 };
 
 export { getRandomNumber, isEven, getRandomMathOperator };
