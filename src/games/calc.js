3fbb6f9e139efdd7b3a0c6a2be53ebafadcb7365
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-03-13 11:08:41 +0300

    Refactoring src/games/calc.js and Fix bin/brain-calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,14 +1,21 @@
-import startGame from '../index.js';
-import { getRandomNumber, getRandomMathOperator } from '../utils.js';
+import { startGame, roundsNumber } from '../index.js';
+import getRandomNumber from '../utils.js';
 
-const calc = () => {
+const getRandomMathOperator = () => {
+  const mathOperators = ['+', '-', '*'];
+  const randomMathOperator = mathOperators[getRandomNumber(0, mathOperators.length - 1)];
+
+  return randomMathOperator;
+};
+
+const runCalcGame = () => {
   const description = 'What is the result of the expression?';
   const questions = [];
   const correctAnswers = [];
 
-  for (let i = 0; i < 3; i += 1) {
-    const firstNumber = getRandomNumber();
-    const secondNumber = getRandomNumber();
+  for (let i = 0; i < roundsNumber; i += 1) {
+    const firstNumber = getRandomNumber(1, 100);
+    const secondNumber = getRandomNumber(1, 100);
     const mathOperator = getRandomMathOperator();
     const question = `${firstNumber} ${mathOperator} ${secondNumber}`;
     let tempAnswer;
@@ -30,4 +37,4 @@ const calc = () => {
   startGame(description, questions, correctAnswers);
 };
 
-export default calc;
+export default runCalcGame;
